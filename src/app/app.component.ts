f498a6220938bbc8d396f1cdefcfb80bdaf0f424
eternal <vijay.suthar738@gmail.com>
2016-12-15 07:24:15 +0530

    create link with image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,9 @@ import { User } from 'app/user';
 })
 export class AppComponent {
   title = 'app works!';
-  outputPath:string = 'www.google.com';
-  new_selectedUser:User;
-  users:User[] = [
+  new_selectedUser : User;
+  demo_link:any = 'www.google.com';
+  users : User[] = [
                     {'id':1,'name':'vijay',phone_no:7854236514,'image':'http://www.american.edu/uploads/profiles/large/chris_palmer_profile_11.jpg'},
                     {'id':2,'name':'parshav',phone_no:8965214569,'image':'https://organicthemes.com/demo/profile/files/2012/12/profile_img.png'},
                     {'id':3,'name':'vicky',phone_no:8512365214,'image':'http://www.gantrypark.com/Portals/12/Users/034/82/53282/michael-asmar.jpg'} 
